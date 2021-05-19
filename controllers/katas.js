bb3106923c6e0d09c35bff996d8ca8e253c7597b
davemolk <davemolk@gmail.com>
2021-05-19 05:43:28 -0600

    links to katas are working

--- controllers/katas.js
+++ controllers/katas.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const router = express.Router();
 const db = require("../models");
-const { route } = require("./auth");
 
 router.get("/", async (req, res) => {
   const fetchKatas = await db.kata.findAll();
